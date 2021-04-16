ba9068a5adbdf1e6e4ea2f58a69113900649f7b2
ArFe <ArFe@users.noreply.github.com>
2021-04-16 10:19:25 -0400

    Add ptl aws iot

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+"use strict";
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const aws_iot_device_sdk_v2_1 = require("aws-iot-device-sdk-v2");
+const util_1 = require("util");
 const express = require("express");
 const app = express();
 const http = require('http').Server(app);
@@ -8,15 +25,24 @@ const exphbs = require('express-handlebars');
 require('body-parser-xml')(bodyParser);
 let xmlparser = require('express-xml-bodyparser');
 let needle = require('needle');
+const { argv } = require("yargs");
+
+const endpoint = "a1ancdr6cxlr44-ats.iot.us-east-1.amazonaws.com";
+const ca_file = "AmazonRootCA1.pem";
+const cert = "ab662e52e8-certificate.pem.crt";
+const key = "ab662e52e8-private.pem.key";
+
+const maxErrCnt = 3;
+const errTimeout = 5;
 
 needle.defaults({
     parse_response: false });
 
 let port = process.env.PORT || 80;
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
-//app.use(bodyParser.xml());
-//app.use(xmlparser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.xml());
+app.use(xmlparser());
 
 let SSinfo = [];
 let cmd = "";
@@ -114,14 +140,33 @@ function xml2Obj(xmlData) {
 };
 
 app.get('/', function(req, res){
-  res.render("index");
-  //res.sendFile(__dirname + '/index.html');
+    res.render("index");
+    //res.sendFile(__dirname + '/index.html');
+  });
+
+  
+    
+app.get('/qrcode', function(req, res){
+res.sendFile(__dirname + '/public/qrcode/qrcodegen-input-demo.html');
 });
 
+app.get('/ptl', function(req, res){
+res.sendFile(__dirname + '/views/ptl.html');
+});
+  
+app.post('/ptl', function(req, res){
+    console.log("POST body");
+    console.log(JSON.safeStringify(req.body));
 
-app.get('/qrcode', function(req, res){
-    res.sendFile(__dirname + '/public/qrcode/qrcodegen-input-demo.html');
-  });
+    const values = req.body.values.split("\r\n");
+    const cmd = req.body.cmd + " ";
+    const rsp = req.body.rsp;
+
+    sendMQTTmsg(req.body.dxmid, cmd, values, rsp);
+
+
+    res.sendFile(__dirname + '/views/ptl.html');
+});
   
   
 app.get('/status', function(req, res){
@@ -138,15 +183,20 @@ io.on('connection', function(socket){
     //console.log(msg);
     try{
       msg = JSON.parse(msg);
+      console.log(msg.topic);
     } catch(err){
       console.log("Error: " + err);
       console.log("Msg not able to be parsed: " + msg);
     }
     if(msg instanceof Object && 'dxmId' in msg){
-      SSinfo.push(msg);
-      //console.log("yes, I have dxmId property");
+        SSinfo.push(msg);
+        console.log("yes, I have dxmId property");
     }
-  });
+
+    if(msg instanceof Object && 'topic' in msg && 'msg' in msg){
+        sendMQTTmsg(msg.topic, msg.msg);
+    }
+    });
 });
 
 // setup a GET 'route' to listen on /push
@@ -196,5 +246,128 @@ app.post("/push", function(req,res){
 });
 
 http.listen(port, function(){
+//   main(process.argv);
   console.log('listening on *:' + port);
 });
+
+process.on('exit', function(code) {
+    return console.log(`About to exit with code ${code}`);
+});
+
+async function sendMQTTmsg(topic, cmd, values, rsp) {
+
+    argv['endpoint'] = endpoint;
+    argv['ca_file'] = ca_file;
+    argv['cert'] = cert;
+    argv['key'] = key;
+    // argv['client_id'] = "ptlApp";
+    argv['topic'] = topic;
+    argv['count'] = 1;
+    argv['use_websocket'] = false;
+    argv['signing_region'] = "us-east-1";
+    // argv['proxy_host'] = null;
+    argv['proxy_port'] = 8080;
+    argv['verbosity'] = 'none';
+
+    argv['cmd'] = cmd;
+    argv['values'] = values;
+    argv['rsp'] = rsp;
+
+    main(argv);
+    
+};
+
+function execute_session(connection, topic, cmd, value, rsp) {
+    const topicsub = topic +"/response"; 
+    const topicpub = topic +"/request"; 
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                const decoder = new util_1.TextDecoder('utf8');
+                const on_publish = (topic, payload, dup, qos, retain) => __awaiter(this, void 0, void 0, function* () {
+                    const json = decoder.decode(payload);
+                    console.log(`Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`);
+                    console.log(json);
+                    const message = JSON.parse(json);
+                    if (message.api.includes(rsp)) {
+                        resolve(rsp);
+                    }
+                });
+                yield connection.subscribe(topicsub, aws_iot_device_sdk_v2_1.mqtt.QoS.AtMostOnce, on_publish);
+                const publish = () => __awaiter(this, void 0, void 0, function* () {
+                    const msg = {
+                        api: cmd + " " + value
+                    };
+                    const json = JSON.stringify(msg);
+                    connection.publish(topicpub, json, aws_iot_device_sdk_v2_1.mqtt.QoS.AtMostOnce);
+                });
+                // publish;
+                setTimeout(publish, 1000);
+                setTimeout(function(){ reject("timeout"); }, errTimeout * 1000);
+                
+            }
+            catch (error) {
+                reject(error);
+            }
+        }));
+    });
+}
+function main(argv) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (argv.verbosity != 'none') {
+            const level = parseInt(aws_iot_device_sdk_v2_1.io.LogLevel[argv.verbosity.toUpperCase()]);
+            aws_iot_device_sdk_v2_1.io.enable_logging(level);
+        }
+        const client_bootstrap = new aws_iot_device_sdk_v2_1.io.ClientBootstrap();
+        let config_builder = null;
+        if (argv.use_websocket) {
+            let proxy_options = undefined;
+            if (argv.proxy_host) {
+                proxy_options = new aws_iot_device_sdk_v2_1.http.HttpProxyOptions(argv.proxy_host, argv.proxy_port);
+            }
+            config_builder = aws_iot_device_sdk_v2_1.iot.AwsIotMqttConnectionConfigBuilder.new_with_websockets({
+                region: argv.signing_region,
+                credentials_provider: aws_iot_device_sdk_v2_1.auth.AwsCredentialsProvider.newDefault(client_bootstrap),
+                proxy_options: proxy_options
+            });
+        }
+        else {
+            config_builder = aws_iot_device_sdk_v2_1.iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(argv.cert, argv.key);
+        }
+        if (argv.ca_file != null) {
+            config_builder.with_certificate_authority_from_path(undefined, argv.ca_file);
+        }
+        config_builder.with_clean_session(false);
+        config_builder.with_client_id(argv.client_id || "test-" + Math.floor(Math.random() * 100000000));
+        config_builder.with_endpoint(argv.endpoint);
+        // force node to wait 60 seconds before killing itself, promises do not keep node alive
+        const timer = setTimeout(() => { }, 60 * 1000);
+        const config = config_builder.build();
+        const client = new aws_iot_device_sdk_v2_1.mqtt.MqttClient(client_bootstrap);
+        const connection = client.new_connection(config);
+        yield connection.connect();
+        console.log('After connect');
+        let i = 0
+        let errorCnt = 0
+        while(i < argv.values.length){
+            try {
+                console.log('argv.values[i] = ' + argv.values[i]);
+                let result = yield execute_session(connection, argv.topic, argv.cmd, argv.values[i], argv.rsp);              
+                console.log('result = ' + result);
+                i++;
+                errorCnt = 0
+            } catch (error) {
+                console.log('error = ' + error);
+                if(++errorCnt >= maxErrCnt){
+                    console.log('Max error achieve. Commands failed');
+                    break;
+                }
+            }
+        }
+        yield connection.disconnect();  
+        console.log('After disconnect');
+        // Allow node to die if the promise above resolved
+        clearTimeout(timer);
+    });
+}
+//# sourceMappingURL=index.js.map
